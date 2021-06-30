294473ac767ee78f4a4b7994d85816eba82e25ca
rashaalkyeam <rasha.alqyyam@gmail.com>
2021-06-30 23:18:09 +0300

    lab03

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,16 +2,27 @@ import React from 'react';
 
 
 class Home extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            vote: 0
+        };
+    }
+    votting = () => {
+        this.setState({
+            vote: this.state.vote + 1
+        })
+    }
 render(){
     return(
         <div>
             <h2>{this.props.title}</h2>
-            <img src={this.props.imge_url} alt={this.props.title}/>
-            <p>{this.props.descr}</p>
+            <img onClick={this.votting} src={this.props.image_url} alt={this.props.title}/>
+            <p>{this.props.description}</p>
+            <p>❤️ {this.state.vote}</p>
+            
         </div>
     );
 }
-
 }
-
 export default Home;
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,38 +1,61 @@
 import React, { component } from 'react'
 import Home from './Home'
+import Horned from './Horned.json';
+import { Form, Button } from 'react-bootstrap/';
+import FormInfoModal from './FormInfoModal';
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            horns:'',
+            showModal: false
+        }
+    }
+
+
+
+    handleClose = () =>{
+        this.setState({
+            showModal:false
+        })
+    }
+    submitForm = (event) =>{
+        event.preventDefault();
+        this.setState ({
+            
+            horns: event.target.horns.value,
+            showModal:true
+        })}
     render() {
-        let home = [
-            {
-                "image_url": "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg",
-                "title": "UniWhal",
-                "descr": "A unicorn and a narwhal nuzzling their horns",
-                "keyword": "narwhal",
-                "horns": 1
-            },
-            {
-                "image_url": "https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80",
-                "title": "Rhino Family",
-                "descr": "Mother (or father) rhino with two babies",
-                "keyword": "rhino",
-                "horns": 2
-            },
-            {
-                "image_url": "https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg",
-                "title": "Unicorn Head",
-                "descr": "Someone wearing a creepy unicorn head mask",
-                "keyword": "unicorn",
-                "horns": 1
-            }
-        ]
-        return (
+        return(
             <div>
-                <Home title={home[0].title} descr={home[0].descr} image_url={home[0].image_url} />
-                <Home title={home[1].title} descr={home[1].descr} image_url={home[1].image_url} />
-                <Home title={home[2].title} descr={home[2].descr} image_url={home[2].image_url} />
-            </div>
-        );
+                <Form onSubmit={this.submitForm}>
+                    <Form.Group aria-label="Default select example">
+                        <Form.Label>choose your favorite cat breed</Form.Label>
+                        <Form.Control as='select' name='horns'>
+                        <option>All </option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="100">wow...</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Button  variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+
+
+        {Horned.map((item)=>{
+            return(
+            <Home Home title={item.title} description={item.description} image_url={item.image_url} />
+            )
+        })} 
+        </div>
+    
+        )
+       
     }
 }
 
